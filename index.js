8c96e8dc18c3672129ad34a81af8f8036f818f3b
Majed Amin <majedamin974@gmail.com>
2021-02-13 22:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let arr = [1, 3, 5];
 function breakOut(array, changeValue, stopValue) {
-
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;
+    }
+    array[i] = array[i] + 3;
+  }
+  return array;
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  
 }
 
 function findBy(array, findFn) {
