44381911ce14b5ff3b0105270e528a2943f0e56f
Majed Amin <majedamin974@gmail.com>
2021-02-13 22:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
-    debugging;
+    debugger;
     if (findFn(array[i])) {
       return array[i];
     } else {
