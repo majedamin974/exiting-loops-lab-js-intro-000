58a81a2f50dc1553dc9f0d769078ef0226be307d
Majed Amin <majedamin974@gmail.com>
2021-02-13 22:34:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
       return array[i];
+    } else {
+      return null;
     }
-    return null;
+    
   }
 }
 
